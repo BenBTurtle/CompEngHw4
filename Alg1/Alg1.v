9adb5c655015c53ce0702c4a51d82b3dbd0fa892
BenBTurtle <benbturtle@gmail.com>
2025-03-10 17:02:35 -0400

    Finished base implementation of algorithm 1, just needs some testing when an FPGA is available

--- Alg1/Alg1.v
+++ Alg1/Alg1.v
@@ -1,25 +1,66 @@
-module Alg1(input clk,
-input [size-1:0] aIn, bIn, //32 bit inputs and outputs for testing
-output reg [size-1:0] result);
+module Alg1(input clk, load, aOrb,
+input [size-1:0] currIn, //32 bit inputs and outputs for testing
+output [0:(size*2)-1] result,
+output [7:0] disp0, disp1, disp2, disp3);
 
-parameter size = 32;// change this to the width of the operation in bits
+parameter size = 8;// change this to the width of the operation in bits
+
+reg [size-1:0] a;
+reg [size-1:0] b;
+
+initial begin
+	a = 0;
+	b = 0;
+	//result = 0;
+end
+
+always @(posedge clk) begin
+	
+	a = (aOrb == 0)? currIn : a;
+	b = (aOrb == 1)? currIn : b;
+	
+end
+
+mul1 mult(.clk(clk), .load(load), .aIn(a), .bIn(b), .result(result));
+
+hexSevenSegmentDecoder h0(result[0:3],disp3);
+hexSevenSegmentDecoder h1(result[4:7],disp2);
+hexSevenSegmentDecoder h2(result[8:11],disp1);
+hexSevenSegmentDecoder h3(result[12:15],disp0);
+
+endmodule
+
+
+
+module mul1 (input clk, load,
+input [size-1:0] aIn, bIn, //32 bit inputs and outputs for testing
+output reg [(size*2)-1:0] result);
+
+parameter size = 8;// change this to the width of the operation in bits
 
 reg [size-1:0] s;
 reg [size-1:0] a;
 reg [size-1:0] b;
 initial begin //set initial state
 	s = 0;
-	a = aIn;
-	b = bIn;
+	a = 0;
+	b = 0;
 end
 
 always @(posedge clk) begin //posedge to prevent clock related computation errors
-	if (b==0) begin
-		result = s;
-	end else begin
-		s = s+a; b = b-1;
-	end
-	
+	case (load)
+		1'b1 : begin
+			if (b==0) begin
+				result = s;
+			end else begin
+				s = s+a; b = b-1;
+			end
+		end 1'b0 : begin
+			s = 0;
+			a = aIn;
+			b = bIn;
+		end
+	endcase
 end
 
 endmodule
